bfa6cb7124f4904d6a44c0996204167ecee1d912
Ahmed.M Youssef <ahmed.muhammed.youssef@gmail.com>
2025-07-30 21:22:12 +0300

    feat: add new tables

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieLib.Enities;
+
+namespace MovieLib.Database;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<Movie> Movies {  get; set; }
+    public DbSet<Genre> Genres { get; set; }
+}
