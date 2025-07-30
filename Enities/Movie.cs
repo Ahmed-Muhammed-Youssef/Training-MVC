27e1118d74969130f3f16bac9de2e0edb623418b
Ahmed.M Youssef <ahmed.muhammed.youssef@gmail.com>
2025-07-30 20:35:17 +0300

    init commit

---
+++ Enities/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieLib.Enities;
+
+public class Movie
+{
+    public int Id { get; set; }
+    [Required, MaxLength(250)]
+    public string Title { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public double Rate { get; set; }
+    [Required, MaxLength(2500)]
+    public string StoryLine { get; set; } = string.Empty;
+    public Genre? Genre { get; set; }
+    public byte GenreId { get; set; }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieLib.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
